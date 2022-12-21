2537b7157c337f2c0989ab6ce3e63d028a08402a
Patricia Fiol <patricia.fiol@outlook.com>
2022-12-21 20:51:47 +0100

    add navigation menu

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Greeting Animation
+
 const greetingBubble = document.getElementById('greeting')
 
 let timeNow = new Date().getHours()
@@ -5,3 +7,24 @@ let timeNow = new Date().getHours()
 let greeting = timeNow >= 5 && timeNow < 12 ? 'Good Morning!' : timeNow >= 12 && timeNow <= 18 ? 'Good afternoon!' : 'Good evening!';
 
 greetingBubble.innerHTML = `${greeting}`
+
+
+// Navigation
+
+const primaryNav = document.querySelector('.primary-navigation')
+const mobileNav = document.querySelector('.mobile-navigation')
+const openToggle = document.getElementById('open')
+const closeToggle = document.getElementById('close')
+
+mobileNav.addEventListener('click', () => {
+  const visible = primaryNav.getAttribute('data-visible')
+  if (visible === 'false') {
+    primaryNav.setAttribute('data-visible', true);
+    openToggle.setAttribute('data-visible', false);
+    closeToggle.setAttribute('data-visible', true);
+  } else {
+    primaryNav.setAttribute('data-visible', false);
+    openToggle.setAttribute('data-visible', true);
+    closeToggle.setAttribute('data-visible', false);
+  }
+})
