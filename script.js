b72e209cfc26a9047295e8c12946ec011ddab6d8
Patricia Fiol <patricia.fiol@outlook.com>
2023-02-02 11:07:33 +0100

    add logo, favicon and check performance

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-var docWidth = document.documentElement.offsetWidth;
-
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
-  }
-);
-
 // Hamburguer Menu
 
 const hamburger = document.querySelector('.nav-list .hamburger');
