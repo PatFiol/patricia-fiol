a4ff102d003d1053b6b7c517537bafcf4ae137d6
Patricia Fiol <patricia.fiol@outlook.com>
2023-02-08 09:31:37 +0100

    test2

--- contact.php
+++ contact.php
@@ -11,7 +11,6 @@ if(isset($_POST['submit'])) {
   $txt = "Through the contact-form from: .$name\n E-Mail: .$email\n Message:\n .$message";
 
   sendEmail($mailTo, $subject, $txt, $headers);
-  or die("Sorry, something went wrong!");
 
   echo `<span id="success">Message sent. Thank you!</span>`
 
