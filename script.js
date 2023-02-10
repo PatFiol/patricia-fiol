00b910bf8874f30e6cc2de93996ddb525cf97d39
Patricia Fiol <patricia.fiol@outlook.com>
2023-02-10 10:01:21 +0100

    trying netlify contact-form submission

--- script.js
+++ script.js
@@ -62,8 +62,8 @@ function validateForm () {
     errorFlag = true;
   }
   if(!errorFlag) {
-    // success.style.display ='block';
-    // success.innerHTML = 'Message sent. Thank you!'
+    success.style.display ='block';
+    success.innerHTML = 'Message sent. Thank you!'
     form.reset()
   }
 }
@@ -76,7 +76,3 @@ function clearMessages() {
   email.classList.remove('error');
   message.classList.remove('error');
 };
-
-
-// Send Message
-
